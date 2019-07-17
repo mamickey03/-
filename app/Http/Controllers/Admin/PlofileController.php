ffac6ba1984d294b2b98a8a55735f6747aee7078
”mami” <mami.09069092175@gmail.com>
2019-07-17 13:59:23 +0000

    mynews

---
+++ app/Http/Controllers/Admin/PlofileController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PlofileController extends Controller
+{
+    
+    public function add()
+    {
+        return view('admin.profile.create');
+    }
+    
+    public function create()
+    {
+        return redirect('adim/profile/create');
+    }
+    
+    public function edit()
+    {
+        return view('admin.profile.edit');
+    }
+    
+    public function update()
+    {
+        return redirect('admin/profile/edit');
+    }
+}
+
+Route::get ('admin/profile/create','Admin\ProfileController@add');
+Route::get ('admin/profile/edit','Admin\ProfileController@edit');
---
+++ app/Http/Controllers/Admin/prctice5.php
@@ -0,0 +1,3 @@
+<?php
+//「http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください。
+Route::get('XXX','AAAController@bbb');
