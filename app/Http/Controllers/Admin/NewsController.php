d550aed6c1ec51d6b7be28ea051b29bf46e21200
”mami” <mami.09069092175@gmail.com>
2019-08-06 12:05:56 +0000

    更新

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\News;
+
 class NewsController extends Controller
 {
     public function add()
@@ -14,7 +16,26 @@ class NewsController extends Controller
   
   public function create(Request $request)
   {
-      return redirect('admin/news/create');
+      
+    $this->validate($request, News::$rules);
+    
+    $news = new News;
+    $form = $request->all();
+    
+    if (isset($form['image'])){
+        $path = $request->file('image')->store('public/image');
+        $news->image_path = basename($path);
+    } else {
+        $news->image_path = null;
+    }
+    
+    unset($form['_token']);
+    unset($form['image']);
+    
+    $news->fill($form);
+    $news->save();
+    
+    return redirect('admin/news/create');
   }
 
 }
