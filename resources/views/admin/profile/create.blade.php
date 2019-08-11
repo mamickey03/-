d4b92ce889a8c8f2d6d3eda68af91ba84a131291
”mami” <mami.09069092175@gmail.com>
2019-08-11 10:28:53 +0000

    更新

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>MyProfile</title>
-    </head>
-    <body>
-        <h1>Myプロフィール作成画面</h1>
-    </body>
-</html>
 @extends('layouts.admin')
 
 @section('title', 'プロフィールの新規作成')
