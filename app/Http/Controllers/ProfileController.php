73d8bc9a17750794c0bf225ad75c1b1070dce73a
”mami” <mami.09069092175@gmail.com>
2020-03-13 06:50:52 +0000

    heroku

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -10,14 +10,14 @@ class ProfileController extends Controller
 {
     public function index(Request $request)
     {
-        $posts = Profile::all()->sortByDesc('updated_at');
+        $profiles = Profile::all()->sortByDesc('updated_at');
 
-        if (count($posts) > 0) {
-            $headline = $posts->shift();
+        if (count($profiles) > 0) {
+            $headline = $profiles->shift();
         } else {
             $headline = null;
         }
 
-        return view('profile.index', ['headline' => $headline, 'posts' => $posts]);
+        return view('profile.index', ['headline' => $headline, 'posts' => $profiles]);
     }
 }
